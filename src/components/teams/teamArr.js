49269fcce166721c2123e8788c93b97530a0f752
Kotiko <124360315+Kotiko-UA@users.noreply.github.com>
2023-12-25 20:20:02 +0200

    add import photo

--- src/components/teams/teamArr.js
+++ src/components/teams/teamArr.js
@@ -1,51 +1,75 @@
+import aksonov from './teamsphoto/aksonov.jpg';
+import sukhara from './teamsphoto/sukhra.jpg';
+import sharyak from './teamsphoto/sharyak.jpg';
+import shkarupa from './teamsphoto/shkarupa.jpg';
+import zaharova from './teamsphoto/zaharova.jpg';
+import parkhomenko from './teamsphoto/parkhomenko.jpg';
+import vovk from './teamsphoto/vovk.jpg';
+// import aksonov from './teamsphoto/aksonov.jpg'
+// import aksonov from './teamsphoto/aksonov.jpg'
+
 export const teamGroup = [
   {
     id: '1',
     name: 'Aksonov Yevhen',
-    photo: './teamsphoto/aksonov.svg',
+    photo: aksonov,
     link: 'https://www.linkedin.com/in/kotiko/',
     role: 'teamlead',
   },
   {
     id: '2',
     name: 'Viktoriia Sukhara',
-    photo: './teamsphoto/sukhra.svg',
+    photo: sukhara,
     link: 'https://www.linkedin.com/in/viktoriia-sukhara-31616a288/',
     role: 'scram master',
   },
-  {
-    id: '3',
-    name: 'Oleksandr Sharyak',
-    photo: '',
-    link: 'https://www.linkedin.com/in/oleksandr-sharyak-b72a47289/',
-    role: 'developer',
-  },
+  //   {
+  //     id: '3',
+  //     name: 'Oleksandr Sharyak',
+  //     photo: sharyak,
+  //     link: 'https://www.linkedin.com/in/oleksandr-sharyak-b72a47289/',
+  //     role: 'developer',
+  //   },
   {
     id: '4',
     name: 'Konstantin Shkarupa',
-    photo: './teamsphoto/shkarupa.svg',
+    photo: shkarupa,
     link: 'https://www.linkedin.com/in/konstantin-shkarupa/',
     role: 'developer',
   },
   {
     id: '5',
     name: 'Kate Zaharova',
-    photo: './teamsphoto/zaharova.svg',
+    photo: zaharova,
     link: 'https://www.linkedin.com/in/kate-zaharova',
     role: 'developer',
   },
   {
     id: '6',
     name: 'Anastasiia Parkhomenko',
-    photo: '',
+    photo: parkhomenko,
     link: 'https://www.linkedin.com/in/anastasiia-parkhomenko-dev/',
     role: 'developer',
   },
   {
     id: '7',
     name: 'Igor Volk',
-    photo: '',
+    photo: vovk,
     link: 'https://www.linkedin.com/in/igor-volk',
     role: 'developer',
   },
+  //   {
+  //     id: '8',
+  //     name: 'Igor Volk',
+  //     photo: vovk,
+  //     link: 'https://www.linkedin.com/in/igor-volk',
+  //     role: 'developer',
+  //   },
+  //   {
+  //     id: '9',
+  //     name: 'Igor Volk',
+  //     photo: vovk,
+  //     link: 'https://www.linkedin.com/in/igor-volk',
+  //     role: 'developer',
+  //   },
 ];
