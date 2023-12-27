372bcc8cbc64e8823ec838edd1a95912807bea5f
Kotiko <124360315+Kotiko-UA@users.noreply.github.com>
2023-12-27 18:00:53 +0200

    Squashed commit of the following:
    commit 7ec4935de81efedf8ecad4ab3d3e2cc949b557c7
    Merge: daa0627 4d0c411
    Author: Kotiko <124360315+Kotiko-UA@users.noreply.github.com>
    Date:   Wed Dec 27 17:32:48 2023 +0200
    
        Merge pull request #33 from Kotiko-UA/request-avatar-in-login
    
        Request avatar in login
    
    commit 4d0c4111d9ca1049e25b4c8c4d4da4370f0fc474
    Author: VikaSukhara <125195456+VikaSukhara@users.noreply.github.com>
    Date:   Wed Dec 27 16:31:32 2023 +0100
    
        request-in-login
    
    commit daa06272e1e70231aa20f8159f37b827518b7b96
    Merge: 3e494f9 96551f4
    Author: Kotiko <124360315+Kotiko-UA@users.noreply.github.com>
    Date:   Wed Dec 27 17:14:09 2023 +0200
    
        Merge pull request #32 from Kotiko-UA/request-user-name
    
        request
    
    commit 96551f48d8e641f788e267dce610e77e1f306eb1
    Author: VikaSukhara <125195456+VikaSukhara@users.noreply.github.com>
    Date:   Wed Dec 27 15:57:46 2023 +0100
    
        request
    
        request user name
    
    commit 3e494f9100a64fa3b654af4f063250fdda61489d
    Merge: 95d91a4 67bea8b
    Author: Kotiko <124360315+Kotiko-UA@users.noreply.github.com>
    Date:   Wed Dec 27 15:43:01 2023 +0200
    
        Merge pull request #30 from Kotiko-UA/colors-css-home-default-page
    
        colors css defaul page
    
    commit 67bea8b2c272122f3f962bab3dbd39d5f434745a
    Author: VikaSukhara <125195456+VikaSukhara@users.noreply.github.com>
    Date:   Wed Dec 27 14:40:59 2023 +0100
    
        colors css defaul page

--- src/components/HeaderMarkup/HeaderMarkup.styled.js
+++ src/components/HeaderMarkup/HeaderMarkup.styled.js
@@ -3,6 +3,7 @@ import { ReactComponent as Logo } from '../../icons/logo.svg';
 import { ReactComponent as Avatar } from '../../icons/user.svg';
 import { ReactComponent as LogOut } from '../../icons/logout.svg';
 import { ReactComponent as Settings } from '../../icons/settings.svg';
+import { ReactComponent as Arrow } from '../../icons/arrow.svg';
 import { NavLink } from 'react-router-dom';
 
 export const Container = styled.div`
@@ -24,13 +25,13 @@ export const Container = styled.div`
 `;
 
 export const StyledLinkLogo = styled(NavLink)`
-  font-family: Roboto;
   font-size: 12px;
   font-style: normal;
   font-weight: 700;
   line-height: normal;
   text-transform: uppercase;
   color: #407bff;
+  color: ${props => props.theme.primaryBlue};
 
   display: flex;
   align-items: center;
@@ -38,7 +39,7 @@ export const StyledLinkLogo = styled(NavLink)`
   justify-content: flex-end;
 
   &:hover {
-    color: #ff9d43;
+    color: ${props => props.theme.secondaryOrange};
   }
 `;
 
@@ -65,18 +66,17 @@ export const RightNavWrapper = styled.div`
   }
 `;
 export const StyledLink = styled(NavLink)`
-  font-family: Roboto;
   font-size: 18px;
   font-style: normal;
   font-weight: 400;
   line-height: 24px;
-  color: #407bff;
+  color: ${props => props.theme.primaryBlue};
 
   display: flex;
   align-items: center;
   gap: 8px;
   &:hover {
-    color: #ff9d43;
+    color: ${props => props.theme.secondaryOrange};
   }
 `;
 
@@ -86,9 +86,16 @@ export const RealUserIcon = styled.img`
   border-radius: 28px;
 `;
 
+export const ArrowIcon = styled(Arrow)`
+  width: 16px;
+  height: 16px;
+  margin-left: 4px;
+`;
+
 export const UserAvatar = styled(Avatar)`
   width: 28px;
   height: 28px;
+
 `;
 
 export const NavMenu = styled.ul`
@@ -100,10 +107,10 @@ export const NavMenu = styled.ul`
   width: 118px;
 
   border-radius: 10px;
-  background: #fff;
+  background: ${props => props.theme.primaryWhite};
   box-shadow: 0px 4px 8px 0px rgba(64, 123, 255, 0.2);
 
-  color: #407bff;
+  color: ${props => props.theme.primaryBlue};
   font-size: 16px;
   font-style: normal;
   font-weight: 400;
