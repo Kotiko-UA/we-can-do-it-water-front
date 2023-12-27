96c463e4878983196863b5915bae0212cf47f460
Kotiko <124360315+Kotiko-UA@users.noreply.github.com>
2023-12-27 14:34:57 +0200

    add modal state

--- src/Pages/HomeUserPage/HomeUserPage.jsx
+++ src/Pages/HomeUserPage/HomeUserPage.jsx
@@ -2,8 +2,13 @@ import { Container, WrapperHomePage } from './HomeUserPage.styled';
 
 import { MyDailyNorma } from './components/MyDailyNorma/MyDailyNorma';
 import Calendar from './components/Calendar/Calendar';
+import { useState } from 'react';
 
 const HomeUserPage = () => {
+  const [dailyNormaModal, setDailyNormaModal] = useState(false);
+  const [addWaterModal, setAddWaterModal] = useState(false);
+  const [correctWaterModal, setCorrectWaterModal] = useState(false);
+  const [delWaterModal, setDelWaterModal] = useState(false);
   return (
     <Container>
       <WrapperHomePage>
