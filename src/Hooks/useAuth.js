f2407d2db67d794f3b1f6ab06b23552d1a031ba0
VikaSukhara <125195456+VikaSukhara@users.noreply.github.com>
2023-12-27 01:04:21 +0100

    avatar-user-photo
    - add change of the avatar, when user is login
    - add css

--- src/Hooks/useAuth.js
+++ src/Hooks/useAuth.js
@@ -1,14 +1,16 @@
 import { useSelector } from "react-redux";
-import { selectUser, selectIsLoggedIn, selectIsRefreshing } from "../components/redux/auth/selectors.js";
+import { selectUser, selectIsLoggedIn, selectIsRefreshing, selectIcon } from "../components/redux/auth/selectors.js";
 
 export const useAuth = () => {
     const isLoggedIn = useSelector(selectIsLoggedIn);
     const isRefreshing = useSelector(selectIsRefreshing);
     const user = useSelector(selectUser);
+    const userIcon = useSelector(selectIcon)
 
     return {
         isLoggedIn,
         isRefreshing,
         user,
+        userIcon
     };
 };
--- src/components/redux/auth/slice.js
+++ src/components/redux/auth/slice.js
@@ -1,51 +1,56 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { signUp, signIn, refreshUser } from "./operations";
+import { createSlice } from '@reduxjs/toolkit';
+import { signUp, signIn, refreshUser } from './operations';
 
 const initialState = {
-    user: { name: null, email: null },
-    token: null,
-    isLoggedIn: false,
-    isRefreshing: false,
+  user: { name: null, email: null },
+  token: null,
+  isLoggedIn: false,
+  isRefreshing: false,
+  icon: null,
 };
 
 const handleRejected = (state, action) => {
-    alert(action.payload);
+  alert(action.payload);
 };
 
 const authSlice = createSlice({
-    name: "auth",
-    initialState,
-    extraReducers: (builder) => {
+  name: 'auth',
+  initialState,
+  extraReducers: builder => {
     builder
-       .addCase(signUp.fulfilled, (state, action) => {
-            state.user = action.payload.user;
-            state.token = action.payload.token;
-            state.isLoggedIn = true;
-        })
-        .addCase(signIn.fulfilled, (state, action) => {
-            state.user = action.payload.user;
-            state.token = action.payload.token;
-            state.isLoggedIn = true;
-        })
-        .addCase(signIn.rejected, (state, action) => {
-            handleRejected (state, action)
-        })
-        .addCase(signUp.rejected, (state, action) => {
-            handleRejected (state, action)   
-        })
+      .addCase(signUp.fulfilled, (state, action) => {
+        state.user = action.payload.user;
+        state.token = action.payload.token;
+        state.isLoggedIn = true;
+        state.icon = action.payload.avatarURL;
+      })
+      .addCase(signIn.fulfilled, (state, action) => {
+        state.user = action.payload.user;
+        state.token = action.payload.token;
+        state.isLoggedIn = true;
+      })
+      .addCase(signIn.rejected, (state, action) => {
+        handleRejected(state, action);
+      })
+      .addCase(signUp.rejected, (state, action) => {
+        handleRejected(state, action);
+      })
 
-        .addCase(refreshUser.pending, (state, action) => {
-            state.isRefreshing = true;
-        })
-        .addCase(refreshUser.fulfilled, (state, action) => {
-            state.user = action.payload;
-            state.isLoggedIn = true;
-            state.isRefreshing = false;
-        })
-        .addCase(refreshUser.rejected, (state, action) => {
-            state.isRefreshing = false;
-        })
-    },
+      .addCase(refreshUser.pending, (state, action) => {
+        state.isRefreshing = true;
+      })
+      .addCase(refreshUser.fulfilled, (state, action) => {
+        state.user = action.payload;
+        state.isLoggedIn = true;
+        state.isRefreshing = false;
+      })
+      .addCase(refreshUser.rejected, (state, action) => {
+        state.isRefreshing = false;
+      });
+  },
 });
+
+console.log(authSlice);
+// console.log(state.icon)
 
 export const authReducer = authSlice.reducer;
