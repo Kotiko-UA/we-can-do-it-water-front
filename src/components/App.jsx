f07f6c21a5504a3c23019b9daee3d7e316d6d0b6
Kate Zaharova <15071981ktz@gmail.com>
2023-12-29 20:47:53 +0200

    bags check

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,11 +10,12 @@ import HomePage from 'Pages/HomePage/HomePage.jsx';
 import { useDispatch } from "react-redux";
 import { useAuth } from "Hooks";
 import { useEffect /*lazy*/ } from "react";
+import { refreshUser } from "./redux/auth/operations.js";
 
 
 export const App = () => {
   const dispatch = useDispatch();
-  const { isRefreshing } = useAuth();
+  //const { isRefreshing } = useAuth();
 
   useEffect(() => {
     dispatch(refreshUser());
