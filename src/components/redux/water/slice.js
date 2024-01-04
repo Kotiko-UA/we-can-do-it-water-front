3796db8ae384f6c132cab43b21d1c38162e98feb
Kotiko <124360315+Kotiko-UA@users.noreply.github.com>
2024-01-04 20:55:44 +0200

    add fix

--- src/components/redux/water/slice.js
+++ src/components/redux/water/slice.js
@@ -16,35 +16,36 @@ const handleRejected = (state, action) => {
   state.isLoading = false;
   state.error = action.payload;
 };
+
+function isPendingAction(action) {
+  return typeof action.type === 'string' && action.type.endsWith('/pending');
+}
+function isFulfilledAction(action) {
+  return typeof action.type === 'string' && action.type.endsWith('/fulfilled');
+}
+function isRejectedAction(action) {
+  return typeof action.type === 'string' && action.type.endsWith('/rejected');
+}
 const waterSlice = createSlice({
   name: 'water',
   initialState,
   extraReducers: builder => {
     builder
-      .addCase(findWaterToday.pending, state => {
-        state.isLoading = true;
-      })
+      .addCase(findWaterToday.pending, state => {})
       .addCase(findWaterToday.fulfilled, (state, action) => {
-        state.isLoading = false;
         state.error = null;
         state.items = action.payload;
         //"?"
       })
       .addCase(findWaterToday.rejected, handleRejected)
-      .addCase(addWater.pending, state => {
-        state.isLoading = true;
-      })
+      .addCase(addWater.pending, state => {})
       .addCase(addWater.fulfilled, (state, action) => {
-        state.isLoading = false;
         state.error = null;
         state.items.push(action.payload);
       })
       .addCase(addWater.rejected, handleRejected)
-      .addCase(updateWater.pending, state => {
-        state.isLoading = true;
-      })
+      .addCase(updateWater.pending, state => {})
       .addCase(updateWater.fulfilled, (state, action) => {
-        state.isLoading = false;
         state.error = null;
         state.items = state.items.map(water =>
           water.id === action.payload.id ? action.payload : water
@@ -52,11 +53,8 @@ const waterSlice = createSlice({
         //"?"
       })
       .addCase(updateWater.rejected, handleRejected)
-      .addCase(deleteWater.pending, state => {
-        state.isLoading = true;
-      })
+      .addCase(deleteWater.pending, state => {})
       .addCase(deleteWater.fulfilled, (state, action) => {
-        state.isLoading = false;
         state.error = null;
         const index = state.items.findIndex(
           water => water.id === action.payload.id
@@ -67,6 +65,16 @@ const waterSlice = createSlice({
       .addCase(logOut.fulfilled, state => {
         state.items = [];
         state.error = null;
+      })
+      .addMatcher(isPendingAction, (state, action) => {
+        state.isLoading = true;
+        state.error = null;
+      })
+      .addMatcher(isFulfilledAction, (state, action) => {
+        state.isLoading = false;
+        state.error = null;
+      })
+      .addMatcher(isRejectedAction, (state, action) => {
         state.isLoading = false;
       });
   },
