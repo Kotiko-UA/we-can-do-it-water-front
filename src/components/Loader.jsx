20406922069f71e262d4104faa04a17b8f649db0
OleksandrSHAR <125485600+OleksandrSHAR@users.noreply.github.com>
2024-01-04 11:56:35 +0200

    water-modals

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,16 +1,16 @@
-// import { RotatingLines } from 'react-loader-spinner';
-// export const Loader = () => {
-//   return (
-//     <div
-//       style={{ display: 'flex', justifyContent: 'center', marginTop: '300px' }}
-//     >
-//       <RotatingLines
-//         strokeColor="blue"
-//         strokeWidth="5"
-//         animationDuration="0.75"
-//         width="150"
-//         visible={true}
-//       />
-//     </div>
-//   );
-// };
+import { RotatingLines } from 'react-loader-spinner';
+export const Loader = () => {
+  return (
+    <div
+      style={{ display: 'flex', justifyContent: 'center', marginTop: '300px' }}
+    >
+      <RotatingLines
+        strokeColor="blue"
+        strokeWidth="5"
+        animationDuration="0.75"
+        width="150"
+        visible={true}
+      />
+    </div>
+  );
+};
