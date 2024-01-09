66d90429f8973eca51659f93729d5113398d8a77
Ania <124797307+aniakurtiak@users.noreply.github.com>
2024-01-09 16:26:56 +0200

    Update DailyNormaModal.jsx

--- src/components/DailyNormaModal/DailyNormaModal.jsx
+++ src/components/DailyNormaModal/DailyNormaModal.jsx
@@ -60,7 +60,7 @@ export const DailyNormaModal = ({ close }) => {
       .unwrap()
       .then(() => {
         actions.resetForm();
-        closeModal();
+        // closeModal();
       })
       .catch((error) => {
         toast.error('Oops, something went wrong. Please try again');
