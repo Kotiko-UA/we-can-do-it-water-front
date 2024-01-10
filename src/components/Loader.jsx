a894eb95290fdd1a495e2f6d3127987b07d8d2e8
lusya0409 <lusya0409@gmail.com>
2024-01-10 16:20:10 +0200

    fix loader, fix modal body scrolling

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -10,7 +10,7 @@ export const Loader = () => {
         left: '0',
         width: '100%',
         height: '100%',
-        zIndex: '999',
+        zIndex: '1000',
         background: '#fff',
       }}
     >
